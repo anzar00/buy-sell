0497689c57140470841d33b2bf3f6ad305907ed9
Anzar <peerzada.anzar@gmail.com>
2024-12-07 02:32:28 +0530

    added listings page logic

--- buy-sell/src/app/listings-page/listings-page.component.ts
+++ buy-sell/src/app/listings-page/listings-page.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-listings-page',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './listings-page.component.html',
   styleUrl: './listings-page.component.css'
 })
-export class ListingsPageComponent {
+export class ListingsPageComponent implements OnInit {
+  listings : Listing[] = [];
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.listings = fakeListings;
+  }
 }
