ba269b25c70fdc2dcd171ff23fea05985e29d8ff
Anzar <peerzada.anzar@gmail.com>
2024-12-07 02:41:24 +0530

    basic strucutre of listing deatail page addeed

--- buy-sell/src/app/listing-detail-page/listing-detail-page.component.ts
+++ buy-sell/src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-listing-detail-page',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './listing-detail-page.component.html',
   styleUrl: './listing-detail-page.component.css'
 })
+
 export class ListingDetailPageComponent {
+
+  listing!: Listing;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id) || {} as Listing;
+
+    this.route.paramMap.subscribe(params => {
+      console.log(params.get('id'));
+    });
+  }
 
 }
