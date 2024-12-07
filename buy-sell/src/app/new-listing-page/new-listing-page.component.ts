472c5be38fc729b3b54450600296d0b07d563926
Anzar <peerzada.anzar@gmail.com>
2024-12-08 01:14:41 +0530

    add new listing added

--- buy-sell/src/app/new-listing-page/new-listing-page.component.ts
+++ buy-sell/src/app/new-listing-page/new-listing-page.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-listing-page',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './new-listing-page.component.html',
   styleUrl: './new-listing-page.component.css'
 })
-export class NewListingPageComponent {
 
+export class NewListingPageComponent implements OnInit  {
+  
+  name : string = '';
+  description : string = '';  
+  price : number = 0; 
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+      
+  }
+
+  onSubmit() : void {
+    alert('Creating a new listing');
+    this.router.navigateByUrl('/my-listings');
+  }
 }
