e31fd40e194cf99f7ca275b604c9d4ef0df7c7a8
Anzar <peerzada.anzar@gmail.com>
2024-12-08 01:04:34 +0530

    basic my listings pafe added

--- buy-sell/src/app/my-listings-page/my-listings-page.component.ts
+++ buy-sell/src/app/my-listings-page/my-listings-page.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeMyListings } from '../fake-data';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-listings-page',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './my-listings-page.component.html',
   styleUrl: './my-listings-page.component.css'
 })
-export class MyListingsPageComponent {
+export class MyListingsPageComponent implements OnInit {
+  listings: Listing[] = [];
+
+  ngOnInit() : void {
+    this.listings = fakeMyListings;
+  }
+
+  onDeleteClicked(listingId: string) : void {
+    alert(`Deleting listing ${listingId}`);
+  }
 
 }
